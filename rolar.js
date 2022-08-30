15de809063156c82eaa3eb0daa0ff46c0de3b286
Letícia Bomfim <leticiabomfim29@gmail.com>
2022-08-30 15:21:05 -0300

    funçoes javascript

--- rolar.js
+++ rolar.js
@@ -1,6 +1,22 @@
+var obj;
 
 function rolar() {
-    var obj = document.getElementById("div_2").scrollIntoView();
+    obj = document.getElementById("projetos");
+    obj.scrollIntoView();
 }
 
+function rolar_sobre() {
+    obj = document.getElementById("sobre_txt");
+    obj.scrollIntoView();
+}
+
+function rolar_formulario() {
+    obj = document.getElementById("formulario");
+    obj.scrollIntoView();
+}
+
+function rolar_home() {
+    obj = document.getElementById("text-bx");
+    obj.scrollIntoView();
+}
 
